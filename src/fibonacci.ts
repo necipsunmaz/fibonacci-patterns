5f39b2a054bbe233a542b2b9bc69e1eb63832d97
Necip Sunmaz <sunmaznecip@gmail.com>
2022-02-10 23:51:03 +0300

    updated fibonacci numbers algorithm

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,42 +1,20 @@
 export class Fibonacci {
-  private _index: number;
-  public phi: number = 0;
-  public indexPhi: number[] = [];
+  public fibonacciSequences = (n: number) =>
+    new Promise<number[]>(resolve => {
+      n = Math.floor(n);
+      const fibonacciNumbers: number[] = [0, 1];
+      while (fibonacciNumbers.length < n) {
+        fibonacciNumbers.push(fibonacciNumbers[fibonacciNumbers.length - 2] + fibonacciNumbers[fibonacciNumbers.length - 1]);
+      }
 
-  /**
-   *
-   */
-  constructor(index: number) {
-    this._index = index;
-    this.calculatePhi();
-  }
+      resolve(fibonacciNumbers);
+    });
 
-  public fibonacciSequences(sequence: number): number[] {
-    let n1 = 0,
-      n2 = 1,
-      nextTerm: number;
-
-    const fibonacciNumbers: number[] = [];
-    nextTerm = n1 + n2;
-    while (nextTerm <= sequence) {
-      n1 = n2;
-      n2 = nextTerm;
-      nextTerm = n1 + n2;
-      fibonacciNumbers.push(nextTerm);
-    }
-
-    return fibonacciNumbers;
-  }
-
-  public calculatePhi(): number {
-    const numbers = this.fibonacciSequences(this._index);
-
-    for (let i = 0; i < numbers.length; i++) {
-      const ratio = numbers[i + 1] / numbers[i];
-      this.indexPhi.push(ratio);
-      this.phi = ratio;
-    }
-
-    return this.phi;
-  }
+  public calculatePhi = (n: number) =>
+    new Promise<number>(resolve => {
+      this.fibonacciSequences(n).then(numbers => {
+        const phi = numbers[numbers.length - 1] / numbers[numbers.length - 2];
+        resolve(phi);
+      });
+    });
 }
